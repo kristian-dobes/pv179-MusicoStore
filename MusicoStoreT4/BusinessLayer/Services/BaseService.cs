465cf7bd921e8de8b056dd1aae9f23ad471e4ff7
pitris90 <petr.balnar@seznam.cz>
2024-11-19 20:38:47 +0100

    Created BaseService for upcoming refactoring of controllers

---
+++ MusicoStoreT4/BusinessLayer/Services/BaseService.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Services.Interfaces;
+using DataAccessLayer.Data;
+
+namespace BusinessLayer.Services
+{
+    public class BaseService : IBaseService
+    {
+        private readonly MyDBContext _dBContext;
+
+        public BaseService(MyDBContext dBContext)
+        {
+            _dBContext = dBContext;
+        }
+
+        public async Task SaveAsync(bool save)
+        {
+            if (save)
+            {
+                await _dBContext.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ MusicoStoreT4/BusinessLayer/Services/Interfaces/IBaseService.cs
@@ -0,0 +1,7 @@
+﻿namespace BusinessLayer.Services.Interfaces
+{
+    public interface IBaseService
+    {
+        public Task SaveAsync(bool save);
+    }
+}
