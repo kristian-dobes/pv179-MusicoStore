c7a8bc160b95ccc3a3dc4145a46bf3253d32ad87
xzilinek <485115@mail.muni.cz>
2024-11-20 19:37:32 +0100

    Created more tests for the manufacturer facade

--- MusicoStoreT4/Tests/ManufacturerFacadeTests.cs
+++ MusicoStoreT4/Tests/ManufacturerFacadeTests.cs
@@ -40,5 +40,75 @@ namespace Tests
             // Act and Assert //
             Assert.ThrowsAsync<KeyNotFoundException>(async () => await _manufacturerFacade.MergeManufacturersAsync(1, 2));
         }
+
+        [Test]
+        public void MergeManufacturersAsync_ShouldThrowKeyNotFoundException_WhenTargetDoesNotExist()
+        {
+            // Arrange //
+            _manufacturerServiceMock.Setup(s => s.ValidateManufacturerAsync(1)).ReturnsAsync(true);
+            _manufacturerServiceMock.Setup(s => s.ValidateManufacturerAsync(2)).ReturnsAsync(false);
+
+            // Act and Assert //
+            Assert.ThrowsAsync<KeyNotFoundException>(async () => await _manufacturerFacade.MergeManufacturersAsync(1, 2));
+        }
+
+        [Test]
+        public async Task MergeManufacturersAsync_ShouldCallReassignAndDelete_WhenBothExist()
+        {
+            // Arrange //
+            _manufacturerServiceMock.Setup(m => m.ValidateManufacturerAsync(It.IsAny<int>())).ReturnsAsync(true);
+            var sequence = new MockSequence();
+
+            _productServiceMock.InSequence(sequence)
+                .Setup(p => p.ReassignProductsToManufacturerAsync(It.IsAny<int>(), It.IsAny<int>()));
+            _manufacturerServiceMock.InSequence(sequence)
+                .Setup(m => m.DeleteManufacturerAsync(It.IsAny<int>()));
+
+            // Act //
+            await _manufacturerFacade.MergeManufacturersAsync(1, 2);
+
+            // Assert //
+            _manufacturerServiceMock.Verify(m => m.ValidateManufacturerAsync(1), Times.Once);
+            _manufacturerServiceMock.Verify(m => m.ValidateManufacturerAsync(2), Times.Once);
+
+            _productServiceMock.Verify(p => p.ReassignProductsToManufacturerAsync(1, 2), Times.Once);
+            _manufacturerServiceMock.Verify(m => m.DeleteManufacturerAsync(1), Times.Once);
+
+            _productServiceMock.VerifyNoOtherCalls();
+            _manufacturerServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Test]
+        public async Task MergeManufacturersAsync_ShouldReassignAllProductsToTargetManufacturer()
+        {
+            // Arrange //
+            int sourceManufacturerId = 1;
+            int targetManufacturerId = 2;
+
+            _manufacturerServiceMock.Setup(m => m.ValidateManufacturerAsync(sourceManufacturerId)).ReturnsAsync(true);
+            _manufacturerServiceMock.Setup(m => m.ValidateManufacturerAsync(targetManufacturerId)).ReturnsAsync(true);
+
+            var productsToReassign = new List<int> { 101, 102, 103 };
+
+            _productServiceMock.Setup(p => p.ReassignProductsToManufacturerAsync(sourceManufacturerId, targetManufacturerId))
+                .Callback<int, int>((src, tgt) =>
+                {
+                    Assert.AreEqual(sourceManufacturerId, src, "Source manufacturer ID does not match.");
+                    Assert.AreEqual(targetManufacturerId, tgt, "Target manufacturer ID does not match.");
+                })
+                .Returns(Task.CompletedTask);
+
+            // Act //
+            await _manufacturerFacade.MergeManufacturersAsync(sourceManufacturerId, targetManufacturerId);
+
+            // Assert //
+            _manufacturerServiceMock.Verify(m => m.ValidateManufacturerAsync(sourceManufacturerId), Times.Once);
+            _manufacturerServiceMock.Verify(m => m.ValidateManufacturerAsync(targetManufacturerId), Times.Once);
+            _productServiceMock.Verify(p => p.ReassignProductsToManufacturerAsync(sourceManufacturerId, targetManufacturerId), Times.Once);
+            _manufacturerServiceMock.Verify(m => m.DeleteManufacturerAsync(sourceManufacturerId), Times.Once);
+
+            _productServiceMock.VerifyNoOtherCalls();
+            _manufacturerServiceMock.VerifyNoOtherCalls();
+        }
     }
 }
