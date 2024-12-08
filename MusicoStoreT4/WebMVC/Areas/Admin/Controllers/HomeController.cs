f71741dc0cc4be8cb7b1d07822e65af7421e2c21
Kristián Dobeš <xdobes@fi.muni.cz>
2024-12-08 19:42:39 +0100

    introduce admin role and admin section

---
+++ MusicoStoreT4/WebMVC/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebMVC.Area.Admin.Controllers
+{
+    public class HomeController : AdminController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
