58f1cc3783d941a76ed0334b966311a85de69069
xzilinek <485115@mail.muni.cz>
2024-12-20 10:49:16 +0100

    fixed error

--- MusicoStoreT4/BusinessLayer/Services/Interfaces/IImageService.cs
+++ MusicoStoreT4/BusinessLayer/Services/Interfaces/IImageService.cs
@@ -14,7 +14,7 @@ namespace BusinessLayer.Services.Interfaces
     {
         Task<string> GetImagePathByProductIdAsync(int productId);
         Task<Product> CreateImageAsync(Product model, string createdBy);
-        Task<List<FileResult>> GetAllProductsImagesAsync()
+        Task<List<FileResult>> GetAllProductsImagesAsync();
         Task<bool> ChangeOrAssignProductImageAsync(int productId, IFormFile newFile);
         Task<bool> DeleteProductImageAsync(int productId);
         Task<FileResult> GetProductImageAsync(int productId);
